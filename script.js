cb3100096d830a2c4fdb1051c345460141c3901d
ljc-dev <dev.if.ljc@gmail.com>
2020-06-05 20:17:06 +0200

    deleted fonts

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const sessionDecrement = document.getElementById("session-decrement")
 const sessionIncrement = document.getElementById("session-increment")
 const beep = document.getElementById("beep")
 
-
 let interval = null
 let breakTimer = 5, sessionTimer = 25, hasStarted = false,
   isSession = true, timeLeft = 0, mouseDownCounter = 0, btnName = ""
